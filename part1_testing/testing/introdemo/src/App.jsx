1f52a2f713e1c6ecf1a583b7cac971ef476f9ef5
PopiJack <samuelpopjak@outlook.sk>
2025-05-18 23:18:48 +0200

    done the random exercises

--- part1_testing/testing/introdemo/src/App.jsx
+++ part1_testing/testing/introdemo/src/App.jsx
@@ -1,24 +1,37 @@
-const Hello = (props) => {
-  console.log(props)
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-const App = () => {
-  const friends = ['Peter', 'Maya']
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const App = () => {
+  const [value, setValue] = useState(10)
+
+ const setToValue = (newValue) => () => {
+  console.log('value now', newValue)
+  setValue(newValue)
+ }
   return (
     <div>
-      <p>{friends}</p>
+      {value}
+      <Button onClick={setToValue(1000)} text='thousand' />
+      <Button onClick={setToValue(0)} text='reset' />
+      <Button onClick={setToValue(value + 1)} text='increment' />
     </div>
   )
-      
-      
 }
 
 export default App
