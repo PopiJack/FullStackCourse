7c76212b3a98b1061cba9bbd1aad492b860a89b0
PopiJack <samuelpopjak@outlook.sk>
2025-05-21 18:11:02 +0200

    done exercises form reading

--- part2_testing/introdemo/src/App.jsx
+++ part2_testing/introdemo/src/App.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -9,25 +10,36 @@ const App = () => {
 
   const hook = () => {
     console.log('effect')
-    axios
-    .get('http://localhost:3001/notes')
-    .then(response => {
-        console.log('promise fullfilled')
+    noteService
+      .getAll()
+      .then(response => {
         setNotes(response.data)
       })
   }
 
-  const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
+  const toggleImportanceOf = id => {
     const note = notes.find (n => n.id === id)
-    const changeNote = {...note, important: !note.important}
+    const changedNote = {...note, important: !note.important}
 
-    axios.put(url, changeNote).then(response => {
-      setNotes(notes.map(note => note.id === id ? response.data : note))
-    })
-  }
+    noteService 
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server` 
+        )
+        setNotes(notes.filter (n => n.id !== id))
+      })
+    }
 
-  useEffect(hook, [])
+  useEffect(()=> {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
 
   console.log('render', notes.length, 'notes')
 
@@ -38,11 +50,11 @@ const App = () => {
       important: Math.random() < 0.5,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
-        setNewNote('')
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+          setNotes(notes.concat(returnedNote))
+          setNewNote('')
       })
   }
 
