aaea80ed6f3ee8549b4f9466236302b2f5ff60f1
PopiJack <samuelpopjak@outlook.sk>
2025-05-26 23:56:51 +0200

    needed to repair phonebook exercise

--- part2/phonebook/introdemo/src/App.jsx
+++ part2/phonebook/introdemo/src/App.jsx
@@ -174,7 +174,7 @@ const App = () => {
   const peopleToShow = showAll 
     ? persons 
     : persons.filter (person => {
-      return person.name.toLowerCase().trim().includes(searchName)
+      return person.name.toLowerCase().trim().includes(searchName.toLowerCase())
     })
 
   return (
