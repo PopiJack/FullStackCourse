161f4016acd98db151171b07ef733a0c00a6e86f
PopiJack <samuelpopjak@outlook.sk>
2025-06-24 10:49:06 +0200

    changing port

--- part3_testing/index.js
+++ part3_testing/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -9,6 +10,7 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.json())
+app.use(cors())
 app.use(requestLogger)
 
 
@@ -96,7 +98,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
