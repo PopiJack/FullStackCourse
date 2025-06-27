a9c6b6fa54d6635e1be795a24111d5ba210ed99a
PopiJack <samuelpopjak@outlook.sk>
2025-06-27 15:37:00 +0200

    finished instructions before part 3.13

--- part3_testing/index.js
+++ part3_testing/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
+const Note = require('./models/note')
+
 const app = express()
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -10,27 +12,8 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.json())
-app.use(cors())
 app.use(requestLogger)
 app.use(express.static('dist'))
-
-const mongoose = require('mongoose')
-
-const password = process.argv[2]
-const url = `mongodb+srv://fullstack:${password}@cluster0.hrucf4n.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-
 
 let notes = [
     {
@@ -51,9 +34,20 @@ let notes = [
 ]
 
 app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
-    response.json(note)
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({error: 'content missing'})
+    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/', (request, response) => {
@@ -61,18 +55,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find ({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find (note => note.id === id)
-    
-    if (note) {
+    Note.findById (request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).status(`Notes does not match ${id}`).end()
-    }
+    })
 })
 
 const generateId = () => {
@@ -116,7 +107,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
