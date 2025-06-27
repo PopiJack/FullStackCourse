de968ded5ed94710cae7d10c72fb2fcd35367f7e
PopiJack <samuelpopjak@outlook.sk>
2025-06-27 20:00:57 +0200

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -22,16 +22,45 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById (request.params.id).then(person => {
-        response.json(person)
+    Person.findById (request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(400).send({error: 'malformatted id'})
     })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+    console.log(request.params.id)
+    Person.findByIdAndDelete (request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+})
 
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response) => {
+    const { name, number } = request.body
+
+    Person.findById (request.params.id)
+        .then(person => {
+            if (!person) {
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then((updatedPerson) => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
