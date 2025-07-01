b621310211f90e480026494f30762dc0e3863378
PopiJack <samuelpopjak@outlook.sk>
2025-07-01 10:37:28 +0200

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -14,6 +14,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
@@ -70,7 +72,7 @@ app.put('/api/persons/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -84,9 +86,15 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save() 
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => {
+            console.log(error.message)
+            next(error)
+        }
+        )
 })
 
 app.get('/info', (request, response) => {
